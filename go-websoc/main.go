67e54838919251e6c238f05ea8210407057e0073
SohamGhugare <soham.ghugare@gmail.com>
2023-05-22 11:55:41 +0530

    fix: refactored directory

--- go-websoc/main.go
+++ go-websoc/main.go
@@ -13,5 +13,5 @@ func main(){
 	http.Handle("/", websocket.Handler(server.HandleWS))
 
 	fmt.Println("Listening on port 8080...")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe("127.0.0.1:8080", nil)
 }
